8f10ff66770044dc6889b0649e4be31cdd764b9c
Student User <student@workstation.lab.example.com>
2019-10-02 17:34:35 +0000

    test

--- scripts/backbase-cicd-setup.sh
+++ scripts/backbase-cicd-setup.sh
@@ -5,10 +5,16 @@ oc new-project backbase-dev
 oc new-project backbase-qa
 oc new-project backbase-devops
 
-echo ***** Creating Project rolebindings *********
-oc create -f ../templates/backbase-setup-template.yaml
 
 echo ******** Creating Jenkins server *************
-oc new-app jenkins-ephemeral -p MEMORY_LIMIT=2048Mi
+oc new-app jenkins-persistent -p MEMORY_LIMIT=2048Mi
 sleep 10
-oc create -f ../templates/pipeline-bc.yaml 
+echo ***** Creating Project rolebindings *********
+# With admin access, this can be uncommented 
+#oc create -f ../templates/backbase-setup-template.yaml
+oc policy add-role-to-user edit system:serviceaccount:backbase-devops:jenkins -n backbase-dev
+oc policy add-role-to-user edit system:serviceaccount:backbase-devops:jenkins -n backbase-qa
+oc project backbase-dev
+oc policy add-role-to-group system:image-puller system:serviceaccounts:backbase-qa -n backbase-dev
+oc project backbase-devops
+oc process -f ../templates/pipeline-bc.yaml | oc create -f -
